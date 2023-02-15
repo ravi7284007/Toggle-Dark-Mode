b9a6114f81db58af3cbe94c4450348a94eb92cad
ravi7284007 <ravi7284007@gmail.com>
2023-02-15 07:03:24 +0530

    some changes in scripts

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const textBox = document.querySelector('#text-box');
 
 const toggleDarkMode = isDark => {
     nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50 %)';
-    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50 %)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
 
     isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
